75b955649c741d0c3d97cc03d22c185a52ca4fa7
samuelrodriguesbrito <mucarb@gmail.com>
2025-02-17 21:46:46 -0300

    Update Conversor

--- conversorb-d.py
+++ conversorb-d.py
@@ -11,20 +11,20 @@ def converter(textoatual):
 class janela(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Binary to Decimal Converter') #Título do programa
+        self.setWindowTitle("Binary to Decimal Converter") #Título do programa
         self.resize(400, 200) #Tamanho da janela
         self.troca = False
 #Botões
         #Botão C
-        self.c = QPushButton('C', self)
+        self.c = QPushButton("C", self)
         self.c.setGeometry(10,140, 80, 50)
         self.c.clicked.connect(self.limpar)
         #Converter
-        self.converter = QPushButton('CONVERT', self)
+        self.converter = QPushButton("CONVERT", self)
         self.converter.setGeometry(90,140, 150, 50)
         self.converter.clicked.connect(self.resultado)
         #Trocar
-        self.mudar = QPushButton('CHANGE', self)
+        self.mudar = QPushButton("CHANGE", self)
         self.mudar.setGeometry(240,140, 150, 50)
         self.mudar.clicked.connect(self.trocar)
         self.mudar.clicked.connect(self.verificar)
@@ -33,24 +33,24 @@ class janela(QWidget):
         self.texto1 = QLineEdit(self)
         self.texto1.setGeometry(10,70, 380, 65)
         self.texto1.setReadOnly(True)
-        self.texto1.setPlaceholderText('Result')
+        self.texto1.setPlaceholderText("Result")
         #Campo 2
         self.texto2 = QLineEdit(self)
         self.texto2.setGeometry(10,0, 380, 65)
-        self.texto2.setPlaceholderText('Binary')
+        self.texto2.setPlaceholderText("Binary")
         regra = QRegularExpression("0|1[01]*")  # Aceita apenas 0 e 1 repetidamente
         validador = QRegularExpressionValidator(regra, self.texto2)
         self.texto2.setValidator(validador)
 #Funções
     #Função de troca
     def trocar(self):
-        self.texto1.setText('')
-        self.texto2.setText('')
+        self.texto1.setText("")
+        self.texto2.setText("")
         if self.troca == True:
-            self.texto2.setPlaceholderText('Binary')
+            self.texto2.setPlaceholderText("Binary")
             self.troca = False
         else:
-            self.texto2.setPlaceholderText('Decimal')
+            self.texto2.setPlaceholderText("Decimal")
             self.troca = True
     #Função números possíveis
     def verificar(self):
@@ -65,8 +65,8 @@ class janela(QWidget):
             self.texto2.setValidator(validador)
     #Função de limpar
     def limpar(self):
-        self.texto1.setText('')
-        self.texto2.setText('')
+        self.texto1.setText("")
+        self.texto2.setText("")
     #Função de resultado
     def resultado(self):
         if self.troca == False:
@@ -81,10 +81,10 @@ class janela(QWidget):
                 resultado += p2
             self.texto1.setText(str(resultado))
         if self.troca == True:
-            binario = ''
+            binario = ""
             textoatual = self.texto2.text()
-            if textoatual == '0':
-                self.texto1.setText('0')
+            if textoatual == "0":
+                self.texto1.setText("0")
             else:
                 cod = int(textoatual)
                 while cod > 0:
